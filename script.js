c7af71495cbf239330b19a99857478aa13e999c5
SebastianLomme <77025210+SebastianLomme@users.noreply.github.com>
2021-08-05 16:19:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const sqauredNumber = function(number1, number2) {
+    sqaureedNum1 = number1 * number1;
+    sqaureedNum2 = number2 * number2;
+    total1 = sqaureedNum1 + sqaureedNum2
+    total2 = total1 *total1
+    return total2
+}
+
+function sqauredNumber2 (number1, number2){
+    sqaureedNum1 = number1 * number1;
+    sqaureedNum2 = number2 * number2;
+    total1 = sqaureedNum1 + sqaureedNum2
+    total3 = total1 *total1
+    return total3
+}
+
+const sqauredNumber3 = (number1, number2) => {
+    sqaureedNum1 = number1 * number1;
+    sqaureedNum2 = number2 * number2;
+    total1 = sqaureedNum1 + sqaureedNum2
+    total3 = total1 *total1
+    return total3
+}
+
+console.log(sqauredNumber(2, 2))
+console.log(sqauredNumber2(2, 2))
+console.log(sqauredNumber3(2, 2))
+
+
